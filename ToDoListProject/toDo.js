977b18643fa6548e63746680ee7978f937bc7196
Shreya <shreyaalpha1@gmail.com>
2025-03-14 15:56:12 +0530

    Update toDo.js

--- ToDoListProject/toDo.js
+++ ToDoListProject/toDo.js
@@ -1,46 +1,33 @@
-let plusButton = document.querySelector(".plus");
 let addWork= document.querySelector(".addWork");
 
     addWork.addEventListener("click", () => {
         let inputTask = document.querySelector("#inputTask").value.trim();
-        if(inputTask!== ""){
+         if(inputTask!== ""){
  let list = document.createElement("div");
 list.classList.add('listStyle');
-list.textContent = inputTask;
-let checkbox = document.createElement("input");
+list.textContent = inputTask;       // text(work toDo mention here, taken from input)
+let checkbox = document.createElement("input"); //creating checkbox
 checkbox.type="checkbox";
-list.appendChild(checkbox);
+checkbox.classList.add("check");
+list.appendChild(checkbox); // append checkbox here
+let deleteBtn = document.createElement("img"); // creating delete image
+deleteBtn.src="./deleteBtn.png";
+deleteBtn.classList.add("delBtn");  
+list.appendChild(deleteBtn);       // appending delete btn on the list div
 list.style.marginTop = "1rem";
 document.body.appendChild(list);
 
 document.querySelector("#inputTask").value="";
 
-checkbox.addEventListener("click", ()=>{
+checkbox.addEventListener("click", ()=>{    //checkbox work
 list.style.opacity="0.5";
-});
-// checkbox.addEventListener("dblclick", () =>{
-//     list.style.opacity="1";
-// });
-        }
+});                        // closed function of checkbox function
+
+deleteBtn.addEventListener("click", ()=>{  //delete btn work
+   list.remove();
+});  // function of delete btn closed here
+         } //if loop closed here
+         else{
+            alert("enter the work first");  // alert is given when there is nothing in input
+         }
     });
-
-// plus button function
-plusButton.addEventListener("click", ()=>{
-    let inputTask = document.querySelector("#inputTask").value.trim();
-    if(inputTask!== ""){
-    let list = document.createElement("div");
-list.classList.add('listStyle');
-list.style.marginTop = "1rem";
-list.textContent = inputTask;
-document.body.appendChild(list);
-document.querySelector("#inputTask").value="";
-    }
-});
- // let listContainer = document.querySelector("#taskList");
-            // if(!listContainer){
-            //     listContainer=document.createElement("div");
-            //     listContainer.id="taskList";
-            //     document.body.appendChild(listContainer);
-            // }
-
-        // let list = document.createElement("div");
